fd6fd46df6f37279e3e676cda0adf41a22074f22
ervinMamutov <ervin.news@gmail.com>
2023-08-14 13:46:14 +0200

    finish 05-thinking parallel

--- 05-thinking-parallel/2-promise-dot-all/exercises/1-contact-info.test.js
+++ 05-thinking-parallel/2-promise-dot-all/exercises/1-contact-info.test.js
@@ -3,36 +3,42 @@ import { fetchUserById } from '../../../lib/fetch-user-by-id/index.js';
 /**
  *
  */
-const getIntros = async (ids = []) => {};
+const getIntros = async (ids = []) => {
+  const userData = ids.map((id) => fetchUserById(id));
+  const users = await Promise.all(userData);
+  return users.map(
+    (user, index) => `${ids[index]}: Hello, my name is ${user.name}`,
+  );
+};
 
 // --- --- tests --- ---
 
 describe('getIntros: returns an array of user introductions', () => {
-    it('gets intros for 6', async () => {
-        const actual = await getIntros([6]);
-        expect(actual).toEqual(['6: Hello, my name is Mrs. Dennis Schulist']);
-    });
-    it('gets intros for 8, 6, 4', async () => {
-        const actual = await getIntros([8, 6, 4]);
-        expect(actual).toEqual([
-            '8: Hello, my name is Nicholas Runolfsdottir V',
-            '6: Hello, my name is Mrs. Dennis Schulist',
-            '4: Hello, my name is Patricia Lebsack',
-        ]);
-    });
-    it('gets intros for 4, 7, 5, 6', async () => {
-        const actual = await getIntros([4, 7, 5, 6]);
-        expect(actual).toEqual([
-            '4: Hello, my name is Patricia Lebsack',
-            '7: Hello, my name is Kurtis Weissnat',
-            '5: Hello, my name is Chelsey Dietrich',
-            '6: Hello, my name is Mrs. Dennis Schulist',
-        ]);
-    });
-    it('gets intros for no one', async () => {
-        const actual = await getIntros([]);
-        expect(actual).toEqual([]);
-    });
+  it('gets intros for 6', async () => {
+    const actual = await getIntros([6]);
+    expect(actual).toEqual(['6: Hello, my name is Mrs. Dennis Schulist']);
+  });
+  it('gets intros for 8, 6, 4', async () => {
+    const actual = await getIntros([8, 6, 4]);
+    expect(actual).toEqual([
+      '8: Hello, my name is Nicholas Runolfsdottir V',
+      '6: Hello, my name is Mrs. Dennis Schulist',
+      '4: Hello, my name is Patricia Lebsack',
+    ]);
+  });
+  it('gets intros for 4, 7, 5, 6', async () => {
+    const actual = await getIntros([4, 7, 5, 6]);
+    expect(actual).toEqual([
+      '4: Hello, my name is Patricia Lebsack',
+      '7: Hello, my name is Kurtis Weissnat',
+      '5: Hello, my name is Chelsey Dietrich',
+      '6: Hello, my name is Mrs. Dennis Schulist',
+    ]);
+  });
+  it('gets intros for no one', async () => {
+    const actual = await getIntros([]);
+    expect(actual).toEqual([]);
+  });
 });
 
 console.log('= = = =  the call stack is empty  = = = =');
